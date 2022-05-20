6458006d93965c7927f8ea6ef72128c4a01a1808
Awoked <alper.kossay@gmail.com>
2022-05-20 14:33:31 +0300

    v2

---
+++ js/RandomAnimeGenerator/randomizerindex.js
@@ -0,0 +1,72 @@
+function uret(min,max){
+    var sayi= (Math.floor(Math.random() * (max - min + 1)) + min);
+    if (sayi ==1) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Jujutsu Kaisen Movie 0/images.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Jujutsu Kaisen 0</a></div></div>";
+    }else if (sayi ==2) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Sakurasou no pet kanojo/Sakura-sō_no_Pet_na_Kanojo_light_novel_volume_1_cover.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Sakura-sō no Pet na Kanojo</a></div></div>";
+    }else if (sayi ==3) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/HunterXHunter/images.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Hunter x Hunter</a></div></div>";
+    }else if (sayi ==4) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Another/MV5BNWYzNmZmMTUtNzI1YS00NTYwLWIxYWQtMThmYjY1Y2MwZDc2XkEyXkFqcGdeQXVyNDgyODgxNjE@._V1_.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Another</a></div></div>";
+    }else if (sayi ==5) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Given/71c1-xwd-pl.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Given</a></div></div>";
+    }else if (sayi ==6) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/One Piece/MV5BODcwNWE3OTMtMDc3MS00NDFjLWE1OTAtNDU3NjgxODMxY2UyXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>One Piece</a></div></div>";
+    }else if (sayi ==7) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/MY HERO ACADEMİA/MV5BNTk1NTc0MTYtY2IyNC00OWVjLWJhYWItNDQ0ODdiNWZkYTA5XkEyXkFqcGdeQXVyMTQ3MjMyMTYz._V1_FMjpg_UX1000_.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>My Hero Academia:Boku No Hero Academia</a></div></div>";
+    }else if (sayi ==7) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Naruto/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Naruto</a></div></div>";
+    }else if (sayi ==8) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Viloet evergarden/images.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Violet Evergarden</a></div></div>";
+    }else if (sayi ==9) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Demon Slayer/MV5BZjZjNzI5MDctY2Y4YS00NmM4LTljMmItZTFkOTExNGI3ODRhXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Demon Slayer:Kimetsu No Yaiba</a></div></div>";
+    }else if (sayi ==10) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/AOT/AttackOnTitan.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Attack On Titan: Shingeki No Kyojin</a></div></div>";
+    }else if (sayi ==11) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Koe no Katachi/MV5BZGRkOGMxYTUtZTBhYS00NzI3LWEzMDQtOWRhMmNjNjJjMzM4XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UY281_CR3,0,190,281_.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Koe no Katachi</a></div></div>";
+    }else if (sayi ==12) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Kimi no Suizou wo Tabetai/images.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Kimi no Suizou wo Tabetai</a></div></div>";
+    }else if (sayi ==13) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Howls Moving Castle/MV5BNmM4YTFmMmItMGE3Yy00MmRkLTlmZGEtMzZlOTQzYjk3MzA2XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Howls Moving Castle</a></div></div>";
+    }else if (sayi ==14) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Violet Evergarden Movie/images.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Violet Evergarden: The Movie</a></div></div>";
+    }else if (sayi ==15) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Kimi no na wa/583827.jpg-c_310_420_x-f_jpg-q_x-xxyxx.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Your Name</a></div></div>";
+    }else if (sayi ==16) {
+        document.getElementById("randomAnimeBackground").classList.add('randombackground');
+        document.getElementById("randomAnimeContainer").classList.add('randomAnimeContainerMaker');
+        document.getElementById("randomAnimeContainer").innerHTML = "<div class='randomanime'><div class='randomanime_top'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'><img src='images/Chuunibyou demo Koi ga Shitai Movie Take On Me/images.jpg'></a></div><div class='randomanime_bottom'><a href='animeler/Attack_On_Titan/AOT_Bolum_1.html'>Chuunibyou demo Koi ga Shitai! Movie: Take On Me</a></div></div>";
+    }
+}
